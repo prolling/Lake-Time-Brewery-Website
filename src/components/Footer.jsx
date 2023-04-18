9e817c7fbaa32f4ea6a3ac5157c0d8f047bb3adb
EllsworthLogan <lte@iastate.edu>
2023-04-18 17:56:26 -0500

    started the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div name="footer">
+      <div name="container">
+        <div name="content1">Hi</div>
+        <div name="content2">Hello</div>
+        <div name="content3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
