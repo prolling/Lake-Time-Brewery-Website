6f6a8d76011f8dca9d4192899b92c7f10e8c526f
prolling <prolling@iastate.edu>
2023-04-18 16:55:11 -0500

    site set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Events from "./components/Events";
+import Merch from "./components/Merch";
+import Beers from "./components/Beers";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/events">
+            <Events />
+          </Route>
+          <Route path="/merch">
+            <Merch />
+          </Route>
+          <Route path="/beers">
+            <Beers />
+          </Route>
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f2dc98",
+        secondary: "#761e16",
+        tertiary: "#050503",
+        quaternary: "#b39071",
+        quinary: "#564d34",
+      },
+    },
+  },
+  plugins: [],
+};
