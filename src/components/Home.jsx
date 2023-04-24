e8a6f196cb233c3a3c17a9e9665689c0d64a5667
prolling <prolling@iastate.edu>
2023-04-24 11:43:40 -0500

    hero section slider fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,32 +16,26 @@ const Home = () => {
       text: "Find our award winning Peanut Butter Porter at a store near you.",
       button: "Find Our Brews",
       image: hero1,
+      link: "/beers"
     },
     {
       title: "Brewing a Better World",
       text: "We are committed to serving our community and being environmentally friendly through the brewing process.",
       button: "About Lake Time",
       image: hero2,
+      link: "/about"
     },
     {
       title: "Our Taproom",
       text: "Explore our taproom and events calendar.",
       button: "Visit Us",
       image: hero3,
+      link: "/events"
     },
   ];
 
+  // reference to the slider
   const sliderRef = useRef(null);
-
-  // settings for the slider
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false, // Disable default arrows
-  };
 
   const handlePrevClick = () => {
     if (sliderRef.current) {
@@ -54,44 +48,58 @@ const Home = () => {
       sliderRef.current.slickNext();
     }
   };
+  // settings for the slider
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
 
   return (
-    <Slider {...settings}>
-      {heroSections.map((hero, index) => (
-        <div key={index} className="relative">
-          <div className="h-screen flex items-center justify-center bg-black">
-            <div
-              className="absolute top-0 left-0 w-full h-full opacity-70"
-              style={{
-                backgroundImage: `url(${hero.image})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            ></div>
-            <div className="container mx-auto px-4 text-center relative z-10">
-              <h1 className="text-5xl font-bold text-white mb-4">
-                {hero.title}
-              </h1>
-              <p className="text-2xl text-white mb-8">{hero.text}</p>
-              <button className="bg-white text-black py-2 px-4 rounded-full font-medium hover:bg-gray-200 transition duration-300">
-                {hero.button}
-              </button>
-              {/* put slider buttons here */}
-              <div className="flex justify-center mt-4">
-                <FiChevronLeft
-                  className="mx-4 text-white cursor-pointer"
-                  onClick={handlePrevClick}
-                />
-                <FiChevronRight
-                  className="mx-4 text-white cursor-pointer"
-                  onClick={handleNextClick}
-                />
+    // hero section slider here
+    <div>
+      <Slider ref={sliderRef} {...settings}>
+        {heroSections.map((hero, index) => (
+          <div key={index} className="relative">
+            <div className="h-screen flex items-center justify-center bg-black">
+              <div
+                className="absolute top-0 left-0 w-full h-full opacity-70"
+                style={{
+                  backgroundImage: `url(${hero.image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+              <div className="container mx-auto px-4 text-center relative z-10">
+                <h1 className="text-5xl font-bold text-white mb-4">
+                  {hero.title}
+                </h1>
+                <p className="text-2xl text-white mb-8">{hero.text}</p>
+                <Link to={hero.link}>
+                  <button className="bg-white text-black py-2 px-4 rounded-full font-medium hover:bg-gray-200 transition duration-300">
+                    {hero.button}
+                  </button>
+                </Link>
+                {/* put slider buttons here */}
+                <div className="flex justify-center mt-4">
+                  <FiChevronLeft
+                    className="mx-4 text-white cursor-pointer text-4xl"
+                    onClick={handlePrevClick}
+                  />
+                  <FiChevronRight
+                    className="mx-4 text-white cursor-pointer text-4xl"
+                    onClick={handleNextClick}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
