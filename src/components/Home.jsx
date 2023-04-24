e640d8e26bafb68e28bf56f4773acc8f141ddb14
prolling <prolling@iastate.edu>
2023-04-24 12:20:34 -0500

    second section home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,21 +16,21 @@ const Home = () => {
       text: "Find our award winning Peanut Butter Porter at a store near you.",
       button: "Find Our Brews",
       image: hero1,
-      link: "/beers"
+      link: "/beers",
     },
     {
       title: "Brewing a Better World",
       text: "We are committed to serving our community and being environmentally friendly through the brewing process.",
       button: "About Lake Time",
       image: hero2,
-      link: "/about"
+      link: "/about",
     },
     {
       title: "Our Taproom",
       text: "Explore our taproom and events calendar.",
       button: "Visit Us",
       image: hero3,
-      link: "/events"
+      link: "/events",
     },
   ];
 
@@ -61,7 +61,7 @@ const Home = () => {
   return (
     // hero section slider here
     <div>
-      <Slider ref={sliderRef} {...settings}>
+      <Slider ref={sliderRef} {...settings} className="pt-40">
         {heroSections.map((hero, index) => (
           <div key={index} className="relative">
             <div className="h-screen flex items-center justify-center bg-black">
@@ -99,6 +99,54 @@ const Home = () => {
           </div>
         ))}
       </Slider>
+
+      {/* 3 sections here with image backgrounds */}
+      <div className="mx-auto py-2 w-full">
+        <div className="flex flex-wrap">
+          {[
+            {
+              title: "Find Our Brews",
+              // text: "Find your favorite Lake Time brew at a store near you.",
+              image: hero1,
+              link: "/beers",
+            },
+            {
+              title: "Sustainability",
+              // text: "Learn about our brewing process.",
+              image: hero2,
+              link: "/about",
+            },
+            {
+              title: "Our Story",
+              // text: "Learn how we got started",
+              image: hero3,
+              link: "/events",
+            },
+          ].map((item, index) => (
+            <div key={index} className="w-full md:w-1/3 py-1">
+              <Link to={item.link}>
+                <div
+                  className="h-64 rounded-lg bg-cover bg-center relative"
+                  style={{
+                    backgroundImage: `url(${item.image})`,
+                    borderLeft: index !== 0 ? "1px solid white" : "none",
+                    borderRight: index !== 2 ? "1px solid white" : "none",
+                  }}
+                >
+                  <div className="text-2xl hover:text-4xl duration-200 absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 flex flex-col justify-center items-center">
+                    <h2 className=" font-bold text-white flex items-center">
+                      {item.title}
+                    </h2>
+                    {/* <p className="mt-2 text-white flex items-center">
+                {item.text}
+              </p> */}
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
