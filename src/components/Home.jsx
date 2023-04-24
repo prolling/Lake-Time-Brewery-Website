a37c8ea00d93e4ec3f1c2820e9cabd89cff4963a
prolling <prolling@iastate.edu>
2023-04-24 13:08:52 -0500

    section adds to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,10 @@ import hero3 from "../images/deck_beers.jpg";
 import sect11 from "../images/glass.JPG";
 import sect13 from "../images/brewery_chamber.jpg";
 import sect12 from "../images/taproom_outside.jpg";
+import sect21 from "../images/taproom_inside.JPG";
+import sect31 from "../images/careers_brewery.jpg"
+import sect32 from "../images/lots_of_cans.jpg"
+import sect33 from "../images/t-shirts.jpg"
 
 const Home = () => {
   // put in any hero sections here
@@ -62,8 +66,9 @@ const Home = () => {
   };
 
   return (
-    // hero section slider here
+    // home content here
     <div>
+      {/* hero section slider here */}
       <Slider ref={sliderRef} {...settings} className="pt-40">
         {heroSections.map((hero, index) => (
           <div key={index} className="relative">
@@ -149,6 +154,109 @@ const Home = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      {/* one section taproom here */}
+      <div className="mx-auto py-2 w-full">
+        <div className="flex flex-wrap">
+          {[
+            {
+              title: "Taproom",
+              // text: "Find your favorite Lake Time brew at a store near you.",
+              image: sect21,
+              link: "/events",
+              p1: "HOURS: Wednesday 4pm - 9pm <br> Thursday & Friday 4pm - 10 pm <br> Saturday 12pm - 10pm <br> Sunday 12pm - 8pm",
+              p2: "Location: 801 Main Avenue <br> Clear Lake, IA <br> (641) 357-2040",
+            },
+          ].map((item, index) => (
+            <div key={index} className="w-full py-1">
+              <Link to={item.link}>
+                <div
+                  className="h-64 rounded-lg bg-cover bg-center relative"
+                  style={{
+                    backgroundImage: `url(${item.image})`,
+                    borderLeft: index !== 0 ? "1px solid white" : "none",
+                    borderRight: index !== 2 ? "1px solid white" : "none",
+                  }}
+                >
+                  <div className="font-bold text-4xl hover:text-6xl duration-200 absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 flex flex-col justify-center items-center">
+                    <div className="flex flew-row justify-between w-full px-4">
+                      <div
+                        className="mt-2 text-white flex items-center text-xl"
+                        dangerouslySetInnerHTML={{ __html: item.p1 }}
+                      />
+                      <h2 className="font-bold text-white flex items-center">
+                        {item.title}
+                      </h2>
+                      <div
+                        className="font-bold mt-2 text-white flex items-center text-xl"
+                        dangerouslySetInnerHTML={{ __html: item.p2 }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* one section for events here */}
+      <div>
+        <h1>Events</h1>
+      </div>
+
+      {/* 3 sections here with image backgrounds */}
+      <div className="mx-auto py-2 w-full">
+        <div className="flex flex-wrap">
+          {[
+            {
+              title: "Careers",
+              // text: "Find your favorite Lake Time brew at a store near you.",
+              image: sect31,
+              link: "/about",
+            },
+            {
+              title: "Distribution",
+              // text: "Learn about our brewing process.",
+              image: sect32,
+              link: "/about",
+            },
+            {
+              title: "Merch",
+              // text: "Learn how we got started",
+              image: sect33,
+              link: "/merch",
+            },
+          ].map((item, index) => (
+            <div key={index} className="w-full md:w-1/3 py-1">
+              <Link to={item.link}>
+                <div
+                  className="h-64 rounded-lg bg-cover bg-center relative"
+                  style={{
+                    backgroundImage: `url(${item.image})`,
+                    borderLeft: index !== 0 ? "1px solid white" : "none",
+                    borderRight: index !== 2 ? "1px solid white" : "none",
+                  }}
+                >
+                  <div className="text-2xl hover:text-4xl duration-200 absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 flex flex-col justify-center items-center">
+                    <h2 className=" font-bold text-white flex items-center">
+                      {item.title}
+                    </h2>
+                    {/* <p className="mt-2 text-white flex items-center">
+                {item.text}
+              </p> */}
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* one section for social media here */}
+      <div>
+        <h1>Social Media</h1>
       </div>
     </div>
   );
