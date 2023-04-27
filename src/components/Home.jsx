62f421d8bb11c431345116c34174be907754750b
prolling <prolling@iastate.edu>
2023-04-27 14:44:13 -0500

    hero changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import hero1 from "../images/peanut-butter-porter.jpg";
-import hero2 from "../images/kegs.jpg";
-import hero3 from "../images/deck_beers.jpg";
+
 import sect11 from "../images/glass.JPG";
 import sect13 from "../images/brewery_chamber.jpg";
 import sect12 from "../images/taproom_outside.jpg";
@@ -21,97 +18,13 @@ import Hero from "./Home/Hero.jsx"
 
 const Home = () => {
   // put in any hero sections here
-  const heroSections = [
-    {
-      title: "Award Winning Brews",
-      text: "Find our award winning Peanut Butter Porter at a store near you.",
-      button: "Find Our Brews",
-      image: hero1,
-      link: "/beers",
-    },
-    {
-      title: "Brewing a Better World",
-      text: "We are committed to serving our community and being environmentally friendly through the brewing process.",
-      button: "About Lake Time",
-      image: hero2,
-      link: "/about",
-    },
-    {
-      title: "Our Taproom",
-      text: "Explore our taproom and events calendar.",
-      button: "Visit Us",
-      image: hero3,
-      link: "/events",
-    },
-  ];
-
-  // reference to the slider
-  const sliderRef = useRef(null);
-
-  const handlePrevClick = () => {
-    if (sliderRef.current) {
-      sliderRef.current.slickPrev();
-    }
-  };
-
-  const handleNextClick = () => {
-    if (sliderRef.current) {
-      sliderRef.current.slickNext();
-    }
-  };
-  // settings for the slider
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-  };
+  
 
   return (
     // home content here
     <div>
       <Hero />
-      {/* hero section slider here */}
-      <Slider ref={sliderRef} {...settings} className="md:pt-40">
-        {heroSections.map((hero, index) => (
-          <div key={index} className="relative">
-            <div className="h-screen flex items-center justify-center bg-black">
-              <div
-                className="absolute top-0 left-0 w-full h-full opacity-70"
-                style={{
-                  backgroundImage: `url(${hero.image})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
-              ></div>
-              <div className="container mx-auto px-4 text-center relative z-10">
-                <h1 className="text-5xl font-bold text-white mb-4">
-                  {hero.title}
-                </h1>
-                <p className="text-2xl text-white mb-8">{hero.text}</p>
-                <Link to={hero.link}>
-                  <button className="bg-white text-black py-2 px-4 rounded-full font-medium hover:bg-gray-200 transition duration-300">
-                    {hero.button}
-                  </button>
-                </Link>
-                {/* put slider buttons here */}
-                <div className="flex justify-center mt-4">
-                  <FiChevronLeft
-                    className="mx-4 text-white cursor-pointer text-4xl"
-                    onClick={handlePrevClick}
-                  />
-                  <FiChevronRight
-                    className="mx-4 text-white cursor-pointer text-4xl"
-                    onClick={handleNextClick}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </Slider>
+      
 
       {/* 3 sections here with image backgrounds */}
       <div className="mx-auto py-2 w-full">
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,41 +1,207 @@
-import React from 'react';
+// import React, { useRef } from "react";
+// import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import { Link } from "react-router-dom";
+
+// import hero1 from "C:/Users/paige/OneDrive/Desktop/Iowa State/COM S 319/Final/lake-time-website/src/images/peanut-butter-porter.jpg";
+// // import hero2 from "./src/images/kegs.jpg";
+// // import hero3 from "./src/images/deck_beers.jpg";
+
+
+
+// const Hero = () => {
+
+//     const heroSections = [
+//         {
+//           title: "Award Winning Brews",
+//           text: "Find our award winning Peanut Butter Porter at a store near you.",
+//           button: "Find Our Brews",
+//           image: hero1,
+//           link: "/beers",
+//         },
+//         {
+//           title: "Brewing a Better World",
+//           text: "We are committed to serving our community and being environmentally friendly through the brewing process.",
+//           button: "About Lake Time",
+//           image: hero1,
+//           link: "/about",
+//         },
+//         {
+//           title: "Our Taproom",
+//           text: "Explore our taproom and events calendar.",
+//           button: "Visit Us",
+//           image: hero1,
+//           link: "/events",
+//         },
+//       ];
+      
+//       // reference to the slider
+//       const sliderRef = useRef(null);
+      
+//       const handlePrevClick = () => {
+//         if (sliderRef.current) {
+//           sliderRef.current.slickPrev();
+//         }
+//       };
+      
+//       const handleNextClick = () => {
+//         if (sliderRef.current) {
+//           sliderRef.current.slickNext();
+//         }
+//       };
+//       // settings for the slider
+//       const settings = {
+//         dots: false,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: false,
+//       };
+
+//   return (
+//     <Slider ref={sliderRef} {...settings} className="md:pt-40">
+//       {heroSections.map((hero, index) => (
+//         <div key={index} className="relative">
+//           <div className="h-screen flex items-center justify-center bg-black">
+//             <div
+//               className="absolute top-0 left-0 w-full h-full opacity-70"
+//               style={{
+//                 backgroundImage: `url(${hero.image})`,
+//                 backgroundSize: "cover",
+//                 backgroundPosition: "center",
+//               }}
+//             ></div>
+//             <div className="container mx-auto px-4 text-center relative z-10">
+//               <h1 className="text-5xl font-bold text-white mb-4">
+//                 {hero.title}
+//               </h1>
+//               <p className="text-2xl text-white mb-8">{hero.text}</p>
+//               <Link to={hero.link}>
+//                 <button className="bg-white text-black py-2 px-4 rounded-full font-medium hover:bg-gray-200 transition duration-300">
+//                   {hero.button}
+//                 </button>
+//               </Link>
+//               {/* put slider buttons here */}
+//               <div className="flex justify-center mt-4">
+//                 <FiChevronLeft
+//                   className="mx-4 text-white cursor-pointer text-4xl"
+//                   onClick={handlePrevClick}
+//                 />
+//                 <FiChevronRight
+//                   className="mx-4 text-white cursor-pointer text-4xl"
+//                   onClick={handleNextClick}
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </Slider>
+//   );
+// };
+
+// export default Hero;
+
+import React, { useRef } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+import hero1 from "C:/Users/paige/OneDrive/Desktop/Iowa State/COM S 319/Final/lake-time-website/src/images/peanut-butter-porter.jpg";
 
 const Hero = () => {
+  const heroSections = [
+    {
+      title: "Award Winning Brews",
+      text: "Find our award winning Peanut Butter Porter at a store near you.",
+      button: "Find Our Brews",
+      image: hero1,
+      link: "/beers",
+    },
+    {
+      title: "Brewing a Better World",
+      text: "We are committed to serving our community and being environmentally friendly through the brewing process.",
+      button: "About Lake Time",
+      image: hero1,
+      link: "/about",
+    },
+    {
+      title: "Our Taproom",
+      text: "Explore our taproom and events calendar.",
+      button: "Visit Us",
+      image: hero1,
+      link: "/events",
+    },
+  ];
+
+  // reference to the slider
+  const sliderRef = useRef(null);
+
+  const handlePrevClick = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
+  };
+
+  const handleNextClick = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  // settings for the slider
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
-    <>
-      <div className="bg-blue-600 pt-24">
-        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-            <p className="uppercase tracking-loose w-full text-white">What business are you?</p>
-            <h1 className="my-4 text-5xl font-bold leading-tight text-white">
-              Main Hero Message to sell yourself!
-            </h1>
-            <p className="leading-normal text-2xl mb-8 text-white">
-              Sub-hero message, not too long and not too short. Make it just right!
-            </p>
-            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-              Subscribe
-            </button>
+    <Slider ref={sliderRef} {...settings} className="md:pt-40">
+      {heroSections.map((hero, index) => (
+        <div key={index} className="relative">
+          <div className="h-screen flex items-center bg-secondary">
+            <div className="w-1/2 h-full bg-secondary">
+              <img
+                className="rounded-2xl md:w-full md:max-w-[200px]"
+                src={hero.image}
+                alt={hero.title}
+              />
+            </div>
+            <div className="w-1/2 h-full flex flex-col justify-center px-8">
+              <h1 className="text-5xl font-bold text-white mb-4">
+                {hero.title}
+              </h1>
+              <p className="text-2xl text-white mb-8">{hero.text}</p>
+              <Link to={hero.link}>
+                <button className="bg-white text-black py-2 px-4 rounded-full font-medium hover:bg-gray-200 transition duration-300">
+                  {hero.button}
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="w-full md:w-3/5 py-6 text-center">
-            <img className="w-full md:w-4/5 z-50" src="hero.png" alt="hero" />
+          {/* put slider buttons here */}
+          <div className="flex justify-center mt-4 absolute bottom-0 left-0 right-0">
+            <FiChevronLeft
+              className="mx-4 text-white cursor-pointer text-4xl"
+              onClick={handlePrevClick}
+            />
+            <FiChevronRight
+              className="mx-4 text-white cursor-pointer text-4xl"
+              onClick={handleNextClick}
+            />
           </div>
         </div>
-      </div>
-      <div>
-        <svg viewBox="0 0 1440 200" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0,200 C0,200 270,0 720,200 C1170,400 1440,200 1440,200 L1440,0 L0,0 Z" fill="currentColor" />
-        </svg>
-      </div>
-      {/* The rest of your code */}
-    </>
+      ))}
+    </Slider>
   );
 };
 
 export default Hero;
-
-
-
-
-
-
