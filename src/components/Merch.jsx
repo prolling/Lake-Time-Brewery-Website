459901e85d273a6936f26843c818e6b0f92bdc4c
TannerSmith31 <trs3103@iastate.edu>
2023-04-29 17:47:55 -0500

    more edits to merch

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import cap from "../images/merch_images/m_cap.jpeg";
 import gift_pack_pint from "../images/merch_images/m_gift_pack_pint.jpg";
 import lake_time_growler from "../images/merch_images/m_lake_time_growler.jpg";
-import lots_of_shirts from "../images/merch_images/m_lots_of_shirts.jpg";
 import pint_glassware from "../images/merch_images/m_pint_glassware.jpeg";
 import single_gift_pint from "../images/merch_images/m_single_gift_pint.jpg";
-import sweatshirt from "../images/merch_images/sweatshirt.jpeg";
+import sweatshirt from "../images/merch_images/m_sweatshirt.jpeg";
 import t_shirts from "../images/merch_images/t-shirts.jpg";
-import tulips_samplers from "../images/merch_images/tulips_samplers.jpeg";
-import two_boston_rounds from "../images/merch_images/two_boston_rounds.jpg";
+import tulips_samplers from "../images/merch_images/m_tulips_samplers.jpeg";
+import beanie from "../images/merch_images/m_beanie.jpg";
 
 const Merch = () => {
   const merchItems = [
@@ -23,47 +22,66 @@ const Merch = () => {
       text: "this is text is for the pint glass gift pack",
       img: gift_pack_pint,
       imgAlt: "img2",
+    },
+    {
+      title: "Lake Time Growler",
+      text: "this is text for lake time growler",
+      img: lake_time_growler,
+      imgAlt: "img3",
+    },
+    {
+      title: "t-shits",
+      text: "text for tshirts",
+      img: t_shirts,
+      imgAlt: "img4",
+    },
+    {
+      title: "Pint Glassware",
+      text: "text for pint glassware",
+      img: pint_glassware,
+      imgAlt: "img5",
+    },
+    {
+      title: "Single Pint Glass",
+      text: "text for single pint glass",
+      img: single_gift_pint,
+      imgAlt: "img6",
+    },
+    {
+      title: "Lake Time Sweatshirt",
+      text: "text for sweatshirt",
+      img: sweatshirt,
+      imgAlt: "img7",
+    },
+    {
+      title: "Tulip Samplers",
+      text: "text for tulips",
+      img: tulips_samplers,
+      imgAlt: "img8",
+    },
+    {
+      title: "Beanie",
+      text: "text for beanie",
+      img: beanie,
+      imgAlt: "img9",
     }
   ]
   return (
-    <div name="about" className="pt-24 bg-slate-400">
-      <div className="flex items-center justify-evenly gap-y-2 gap-x-4 p-2 flex-wrap">
-        <div className="grid w-1/5 border-4 border-secondary">
-          <img src={cap} alt="Image 1"></img>
-          <p className="border-2 border-secondary text-center bg-primary">Embroidered Cap</p>
-        </div>
-        <div className="grid w-1/5 border-4 border-secondary">
-          <img src={gift_pack_pint} alt="Image 2"></img>
-          <p className="border-2 border-secondary text-center bg-primary">Pint Glass Gift Pack</p>
-        </div>
-        <div className="grid w-1/5 border-4 border-secondary">
-          <img src={lake_time_growler} alt="Image 3"></img>
-          <p className="border-2 border-secondary text-center bg-primary">Lake Time Growler</p>
-        </div>
-        <div className="grid w-1/5 border-4 border-secondary">
-          <img src={lots_of_shirts} alt="Image 4"></img>
-          <p className="border-2 border-secondary text-center bg-primary">t-Shits</p>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={pint_glassware} alt="Image 5"></img>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={single_gift_pint} alt="Image 6"></img>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={sweatshirt} alt="Image 7"></img>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={two_boston_rounds} alt="Image 8"></img>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={tulips_samplers} alt="Image 9"></img>
-        </div>
-        <div className="w-1/4 px-2 mb-4">
-          <img src={t_shirts} alt="Image 10"></img>
+      <div name="about" className="pt-24 bg-slate-400">
+        <div className="flex flex-wrap justify-center items-center gap-4">
+          {merchItems.map((item, index) => (
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+              <div className="bg-white rounded-lg overflow-hidden shadow-lg">
+                <img src={item.img} alt={item.imgAlt} className="w-full" />
+                <div className="p-4">
+                  <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
+                  <p className="text-gray-700">{item.text}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
   );
 };
 
