d2467fb7e71ea5a92cc960802c403cc9a98601bd
TannerSmith31 <trs3103@iastate.edu>
2023-05-01 14:33:47 -0500

    updating 5/1

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import cap from "../images/merch_images/m_cap.jpeg";
-import gift_pack_pint from "../images/merch_images/m_gift_pack_pint.jpg";
 import lake_time_growler from "../images/merch_images/m_lake_time_growler.jpg";
 import pint_glassware from "../images/merch_images/m_pint_glassware.jpeg";
-import single_gift_pint from "../images/merch_images/m_single_gift_pint.jpg";
 import sweatshirt from "../images/merch_images/m_sweatshirt.jpeg";
-import t_shirts from "../images/merch_images/t-shirts.jpg";
+import t_shirts from "../images/merch_images/m_t-shirts.jpg";
 import tulips_samplers from "../images/merch_images/m_tulips_samplers.jpeg";
 import beanie from "../images/merch_images/m_beanie.jpg";
 
@@ -13,76 +11,68 @@ const Merch = () => {
   const merchItems = [
     {
       title: "Embroidered Cap",
-      text: "this is text for the embroidered cap",
+      text: "Lake time brewery offers 2 styles of baseball caps. Each is embroidered with the Lake time logo.",
       img: cap,
-      imgAlt: "img1",
+      imgAlt: "cap image",
     },
     {
-      title: "Pint Glass Gift Pack",
-      text: "this is text is for the pint glass gift pack",
-      img: gift_pack_pint,
-      imgAlt: "img2",
+      title: "Lake Time Stocking Cap",
+      text: "Whether you are planning to go skiing, sledding, or just crawling into your freezer for a quick mental breakdown, this stylish stocking cap brings both style and warmth to any activity.",
+      img: beanie,
+      imgAlt: "beanie image",
+    },
+    {
+      title: "t-shirts",
+      text: "Thats right motherfuckers, we got t-shirts. With a metric fuck-ton of styles to choose from, you could replace your entire wardrobe with soley these and no-one would be any the wiser!",
+      img: t_shirts,
+      imgAlt: "t-shirt image",
+    },
+    {
+      title: "Lake Time Sweatshirt",
+      text: "Are you tired of your nipples getting hard? ",
+      img: sweatshirt,
+      imgAlt: "sweatshirt image",
     },
     {
       title: "Lake Time Growler",
       text: "this is text for lake time growler",
       img: lake_time_growler,
-      imgAlt: "img3",
-    },
-    {
-      title: "t-shits",
-      text: "text for tshirts",
-      img: t_shirts,
-      imgAlt: "img4",
+      imgAlt: "growler image",
     },
     {
       title: "Pint Glassware",
       text: "text for pint glassware",
       img: pint_glassware,
-      imgAlt: "img5",
-    },
-    {
-      title: "Single Pint Glass",
-      text: "text for single pint glass",
-      img: single_gift_pint,
-      imgAlt: "img6",
-    },
-    {
-      title: "Lake Time Sweatshirt",
-      text: "text for sweatshirt",
-      img: sweatshirt,
-      imgAlt: "img7",
+      imgAlt: "pint glassware image",
     },
     {
       title: "Tulip Samplers",
       text: "text for tulips",
       img: tulips_samplers,
-      imgAlt: "img8",
+      imgAlt: "tulip samplers image",
     },
-    {
-      title: "Beanie",
-      text: "text for beanie",
-      img: beanie,
-      imgAlt: "img9",
-    },
-  ];
+  ]
   return (
-    <div name="about" className="pt-24 bg-slate-400">
+    <div name="about" className="pt-24 bg-primary">
       <div className="flex flex-wrap justify-center items-center gap-4">
         {merchItems.map((item, index) => (
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-            <div className="bg-white rounded-lg overflow-hidden shadow-lg">
+          <div className="w-full sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
+            <div className="relative">
               <img src={item.img} alt={item.imgAlt} className="w-full" />
-              <div className="p-4">
-                <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
-                <p className="text-gray-700">{item.text}</p>
+              <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 bg-black hover:bg-opacity-50 hover:opacity-100 transition-opacity duration-300">
+                <div className="overflow-hidden shadow-lg p-4">
+                  <p className="text-white">{item.text}</p>
+                </div>
               </div>
+            </div>
+            <div className="bg-white shadow-lg p-4">
+              <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
             </div>
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 };
 
 export default Merch;
