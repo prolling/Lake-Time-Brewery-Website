d972f3b6fcf24d32bfaefacb9c1e1ff230d48fdc
prolling <prolling@iastate.edu>
2023-05-03 18:46:21 -0500

    paige branch

--- src/components/Beers/BeerSQL.jsx
+++ src/components/Beers/BeerSQL.jsx
@@ -74,10 +74,6 @@ function BeerSQL() {
       <div className="flex flex-wrap justify-center items-center">
         {beers.map((beer) => (
           <BeerCard key={beer.id} beer={beer} />
-          // <div className="w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 group">
-          //   <div key={beer.id}>{beer.name}</div>
-          //   <img src={beer.img} alt={beer.name}></img>
-          // </div>
         ))}
       </div>
 
